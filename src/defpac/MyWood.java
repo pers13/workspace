cf47a504c46a67410b300ab9687c64bf8a8af1c7
pers13 <alex.persian@yandex.ru>
2014-03-12 19:09:15 +0600

    Задача №1
    Задача №1 (без допзадания)

---
+++ src/defpac/MyWood.java
@@ -0,0 +1,91 @@
+package defpac;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyWood implements Wood {
+	private char[][] m_wood;
+	private Map<String, MyWoodman>m_woodmanList;
+	
+	MyWood (char[][] wood){
+		m_wood = wood;
+		m_woodmanList = new HashMap<String, MyWoodman>();
+	}
+	
+	public void createWoodman(String name, Point start) {
+		m_woodmanList.put(name, new MyWoodman(name,start));
+	}
+	public Action move (String name, Direction direction) {
+		if (!m_woodmanList.containsKey(name)) {
+			return Action.WoodmanNotFound;
+		}		
+		Point newLocation = (m_woodmanList.get(name)).GetLocation();
+		if ((newLocation.getX() >= m_wood[0].length)||(newLocation.getX() < 0)||
+		   (newLocation.getY() >= m_wood.length)||(newLocation.getY() < 0))
+		{
+			return Action.Fail;
+		}
+		switch (direction) {
+			case Up: 
+				newLocation = newLocation.MoveUp();
+				break;
+			
+			case Down: 
+				newLocation = newLocation.MoveDown();
+				break;
+			
+			case Left:
+				newLocation = newLocation.MoveLeft();
+				break;
+			
+			case Right: 
+				newLocation = newLocation.MoveRigth();
+				break;
+			
+			case None: 
+				return Action.Ok;
+		}
+		char movedPoint = m_wood[newLocation.getY()][newLocation.getX()];
+		switch(movedPoint) {
+			case '1':
+				return Action.Fail;
+			case '0':
+				(m_woodmanList.get(name)).SetLocation(newLocation);
+				return Action.Ok;
+			case 'K':
+				if ((m_woodmanList.get(name)).Kill()) {
+					(m_woodmanList.get(name)).SetLocation(newLocation);
+					return Action.Dead;
+				}
+				m_woodmanList.remove(name);
+				return Action.WoodmanNotFound;
+			case 'L':
+				(m_woodmanList.get(name)).AddLife();
+				(m_woodmanList.get(name)).SetLocation(newLocation);
+				return Action.Life;
+			default:
+				return null;	
+		}
+	}
+	public boolean equals(Object wood) {
+		if (wood == null) return false;
+		if (wood.getClass() != MyWood.class) return false;
+		MyWood forests = (MyWood) wood;
+		char[][] forest = forests.m_wood;
+		boolean eq = true;
+		for (int j = 0; j < forest[0].length; j++) {
+			for (int i = 0; i < forest.length; i++) {
+				if (forest[j][i] != m_wood[j][i]) { 
+					eq = false;
+					break;
+				}
+			}
+		}
+		return eq;
+		}
+}
+/*
+ после вставания на килл не двигаемся на старт, на нём можно стоять и терять жизни
+ аналогично на лайфе - плюсуем жизни
+ назад с килла необязательно шагать, ход на килл надо таки сделать
+ */
---
+++ src/defpac/MyWoodman.java
@@ -0,0 +1,60 @@
+package defpac;
+
+public class MyWoodman implements Woodman {
+	private String name;
+	private Point location;
+	private Point startLocation;
+	private int lifes = 3;
+	
+	MyWoodman (String m_name, Point start) {
+		name = m_name;
+		startLocation = location = start;
+	}
+	/**
+	 *  оличество жизней персонажа.
+	 */
+	public int GetLifeCount() {
+		return lifes;
+	}
+	/**
+	 * »м€ персонажа.
+	 */
+	public String GetName() {
+		return name;
+	}
+
+	/**
+	 * ”меньшить количество жизней.
+	 * 
+	 * @return успех уменьшени€ жизней.
+	 */
+	public boolean Kill() {
+		lifes--;
+		if (lifes < 0 ) return false;
+		return true;
+	}
+	/**
+	 * ƒобавить жизнь персонажу.
+	 */
+	public void AddLife() {
+		lifes++;
+	}
+	/**
+	 * ћестоположение персонажа на игровом поле.
+	 */
+	public Point GetLocation() {
+		return location;
+	}
+	/**
+	 * ”становить новое местоположение персонажа на игровом поле.
+	 */
+	public void SetLocation(Point new_location) {
+		location = new_location;
+	}
+	/**
+	 * ѕереместить игрока в стартовую точку
+	 */
+	public void MoveToStart() {
+		location = startLocation;
+	}
+}
